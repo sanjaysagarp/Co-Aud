75e93f02a54ebc13468fc5179e15b35867692332
sanjaysagarp <sanjaysagarp@hotmail.com>
2016-02-13 14:29:44 -0800

    initial push

---
+++ app/controllers/home.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//displays the homepage
+
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(app) {
+	app.use('/', router);
+};
+
+router.get('/', function(req, res, next) {
+	res.render('index', {
+		title : 'Welcome!'
+	});
+});
---
+++ server.js
@@ -0,0 +1,60 @@
+"user strict";
+
+var express = require('express'); //the xpress app framework
+var glob = require('glob'); //allows for globbing files by names
+var favicon = require('serve-favicon'); //literally oinly serves favicons
+var morgan = require('morgan'); //logs things to files 
+var bodyParser = require('body-parser'); //parses json, html, etc to html
+var passport = require('passport'); //user authentication
+
+// GOOGLE AND FACEBOOK
+var passport = require('passport');
+var session = require('express-session'); //for user sessions
+//var socket = require('socket.io'); //for back and forth communication
+var fs = require('fs'); //to read files
+var mongoose = require('mongoose');
+var config = require('./secret/config'); //configuration file 
+// var db = require('./app/models'); //mongo schemas
+
+var app = express(); //let's get started!
+
+// persistent login sessions
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+var port = process.env.PORT || 8080;
+
+// connect to a mongoose db, just add to config.db (ex: config.db + '/posts')
+// mongoose.connect(config.db);
+// mongoose.connection.on('error', function(err) {
+// 	console.error(err);
+// });
+
+app.set('views', config.root + 'app/views');
+app.set('view engine', 'jade');
+
+
+//load certificate files
+// var pubCert = fs.readFileSync(config.root + 'security/server-cert.pem', 'utf-8');
+// var privKey = fs.readFileSync(config.root + 'security/server-pvk.pem', 'utf-8');
+
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+// app.use(favicon('public/img/favicon.ico')); // get icon from Theo
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(config.root + '/public'));
+
+//Grabs all the controllers in the folder, and adds them to the controllers
+//list. Synchronous function.
+var controllers = glob.sync(config.root + '/app/controllers/*.js');
+controllers.forEach(function assignController(controller) {
+	require(controller)(app);
+});
+
+// lets get started!
+app.listen(port);
+console.log('server is listening...');
