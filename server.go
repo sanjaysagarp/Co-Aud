be53408ca64dd87b4d6e97e1291becb388fa4608
tchin94 <tchin1994@gmail.com>
2016-04-09 16:10:12 -0700

    added profile, projects, navbar, and role page edited footer, header, and styles.css added temp picture for testing purposes edited server.go to have more page handlers

--- server.go
+++ server.go
@@ -9,7 +9,8 @@ import (
 )
 
 //Compile templates on start
-var templates = template.Must(template.ParseFiles("./app/views/header.html", "./app/views/footer.html", "./app/views/main.html"))
+var templates = template.Must(template.ParseFiles("./app/views/header.html", "./app/views/footer.html", "./app/views/main.html", 
+									"./app/views/profile.html", "./app/views/navbar.html", "./app/views/rolepage.html", "./app/views/projects.html"))
 
 //A Page structure
 type Page struct {
@@ -26,6 +27,18 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	display(w, "main", &Page{Title: "h0i!"})
 }
 
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "profile", &Page{Title: "Profile"})
+}
+
+func rolePageHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "rolepage", &Page{Title: "Role"})
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "projects", &Page{Title: "Projects"})
+}
+
 func main() {
 	rootdir, err := os.Getwd()
 	if err != nil {
@@ -34,7 +47,10 @@ func main() {
 	http.Handle("/public/", http.StripPrefix("/public",
 		http.FileServer(http.Dir(path.Join(rootdir, "public/")))))
 	http.HandleFunc("/", mainHandler)
-	
+	http.HandleFunc("/profile/", profileHandler)
+	http.HandleFunc("/role/", rolePageHandler)
+	http.HandleFunc("/projects/", projectsHandler)
+
 	//Listen on port 80
 	fmt.Println("Server is listening on port 80...")
 	http.ListenAndServe(":80", nil)
