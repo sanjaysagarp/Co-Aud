2ae930117b9a53cf63e447d42f3193e5af83521a
Sean Cheong <your_email@example.com>
2016-04-13 18:33:50 -0700

    html pages added

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 
 //Compile templates on start
 var templates = template.Must(template.ParseFiles("./app/views/header.html", "./app/views/footer.html", "./app/views/main.html", 
-									"./app/views/profile.html", "./app/views/navbar.html", "./app/views/rolepage.html", "./app/views/projects.html"))
+									"./app/views/profile.html", "./app/views/navbar.html", "./app/views/rolepage.html", "./app/views/projects.html", "./app/views/editProfile.html", "./app/views/projectPage.html", "./app/views/addWork.html", "./app/views/contestMain.html", "./app/views/submitCasting.html"))
 
 //A Page structure
 type Page struct {
@@ -39,6 +39,23 @@ func projectsHandler(w http.ResponseWriter, r *http.Request) {
 	display(w, "projects", &Page{Title: "Projects"})
 }
 
+func editProfileHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "editProfile", &Page{Title: "Edit Profile"})
+}
+func projectPageHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "projectPage", &Page{Title: "Project Page"})
+}
+func addWorkHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "addWork", &Page{Title: "Add Work"})
+}
+func contestMainHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "contestMain", &Page{Title: "Contest"})
+}
+
+func submitCastingHandler(w http.ResponseWriter, r *http.Request) {
+	display(w, "submitCasting", &Page{Title: "Submit Casting"})
+}
+
 func main() {
 	rootdir, err := os.Getwd()
 	if err != nil {
@@ -50,8 +67,12 @@ func main() {
 	http.HandleFunc("/profile/", profileHandler)
 	http.HandleFunc("/role/", rolePageHandler)
 	http.HandleFunc("/projects/", projectsHandler)
-
+	http.HandleFunc("/editProfile/", editProfileHandler)
+	http.HandleFunc("/projectPage/", projectPageHandler)
+	http.HandleFunc("/addWork/", addWorkHandler)
+	http.HandleFunc("/contestMain/", contestMainHandler)
+	http.HandleFunc("/submitCasting/", submitCastingHandler)
 	//Listen on port 80
-	fmt.Println("Server is listening on port 80...")
-	http.ListenAndServe(":80", nil)
+	fmt.Println("Server is listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
