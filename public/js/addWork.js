7c8c1459df6ef79a67caf8c2a35fd59673215b7b
Sean Cheong <your_email@example.com>
2016-05-11 02:11:12 -0700

    form validation

--- public/js/addWork.js
+++ public/js/addWork.js
@@ -22,7 +22,7 @@ function validateYoutube(id) {
 	var isyouTubeUrl = /^(https?:\/\/)?([^\/]*\.)?youtube\.com\/watch\?([^]*&)?v=\w+(&[^]*)?/i.test(url);
 	if(!isyouTubeUrl) {
 		var div = $("#"+id).closest("div");
-		div.removeClass("has=success");
+		div.removeClass("has-success");
 		$("#glypcn"+id).remove();
 		div.addClass("has-error has-feedback");
 		div.append('<span id="glypcn'+id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
@@ -54,61 +54,68 @@ $(document).ready(
 			if(validateText("description")) {
 				checker++;
 			}
-			console.log($("#castEmail[]").val());
-			if(checker == 4) {
+			$('.castEmails').each(function () {
+				if (this.value == null || this.value == "") {
+					var div = $(this).closest("div");
+					div.removeClass("has=success");
+					//$("#glypcn"+id).remove();
+					div.addClass("has-error has-feedback");
+					//div.append('<span id="glypcn'+id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+				} else {
+					var div = $(this).closest("div");
+					div.removeClass("has-error");
+					div.addClass("has-success");
+					div.addClass("has-feedback");
+					checker++;
+					//$("#glypcn"+id).remove();
+					//div.append('<span id="glypcn'+id +'" class="glyphicon glyphicon-ok form-control-feedback"></span>');
+				}
+			})
+			$('.castRoles').each(function () {
+				if (this.value == null || this.value == "") {
+					var div = $(this).closest("div");
+					div.removeClass("has=success");
+					//$("#glypcn"+id).remove();
+					div.addClass("has-error has-feedback");
+					//div.append('<span id="glypcn'+id + '" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+				} else {
+					var div = $(this).closest("div");
+					div.removeClass("has-error");
+					div.addClass("has-success");
+					div.addClass("has-feedback");
+					checker++;
+					//$("#glypcn"+id).remove();
+					//div.append('<span id="glypcn'+id +'" class="glyphicon glyphicon-ok form-control-feedback"></span>');
+				}
+			})
+			if(checker == 6 + counter) {
 				$("form#workForm").submit();
 			} else {
 				return false;
 			}
 		});
 		
-	var counter = 1;
-	// $('#submitWork').on('click', function(e) {
-	// 	var casts = []; 
-	// 	var castRoles = [];
-	// 	for(var i = 1; i <= counter; i++) {
-	// 		var cast = $('[name="castEmail' + i + '"]').val();
-	// 		casts.push(cast);
-	// 		var castRole = $('[name="castRole' + i + '"]').val();
-	// 		castRoles.push(castRole);
-			
-	// 	}
-	// 	e.preventDefault();
-	// 	$.ajax({
-	// 			method: 'POST',
-	// 			url: "/api/v1/publishWork/",
-	// 			data: {
-	// 				castList : casts.toString(),
-	// 				castRoles : castRoles.toString(),
-	// 				title : $('[name="title"]').val(),
-	// 				URL : $	('[name="url"]').val(),
-	// 				description : $('[name="description"]').val(),
-	// 				shortDescription : $('[name="shortDescription"]').val()
-	// 			},
-	// 			dataType: 'html',
-	// 			success: function(data) {	
-	// 				if(data) {
-	// 					$("#notification").css("display", "block");
-	// 					$("#notification").addClass("alert alert-success");
-	// 					$("#notification").html("Project has been created!");
-	// 					$("#notification").fadeOut( 3000 );
-	// 					//location.href = "/seanTest/";
-	// 					//window.location.href = "/castings.html/"
-	// 					//"/project/id?=" + $('#submitWork').data("id")
-	// 				} 
-					
-	// 			},
-	// 			failure: function(err) {
-	// 				console.log(err);
-	// 			}
-	// 	});
-	// });
-	
-	$('#addMoreCast').on('click', function(e) {
-		counter++;
-		var newdiv = document.createElement('div');
-		newdiv.innerHTML = "Cast Member " + counter + "<br><input type='text' name='castEmail[]'> as <input type='text' name='castRole[]'>";
-		document.getElementById("dynamicInput").appendChild(newdiv);
-	
-	});
-});
+		var counter = 1;
+		$('#addMoreCast').on('click', function(e) {
+			counter++;
+			var newdiv = document.createElement('div');
+			newdiv.className = "pair";
+			newdiv.innerHTML = "Cast Member " + counter + "<br><div class='form-group castForm'><input type='text' class='castEmails form-control' name='castEmail[]'></div> as <div class='form-group castForm'><input type='text' name='castRole[]' class='castRoles form-control'></div>";
+			document.getElementById("dynamicInput").appendChild(newdiv);
+			document.getElementById("removeCast").style.display = 'inline';
+		});
+		$('#removeCast').on('click', function(e) {
+			if(counter == 2) {
+				counter--;
+				var inputs = $(".pair");
+				$(inputs[inputs.length - 1]).remove();
+					document.getElementById("removeCast").style.display = 'none';
+				return false;
+				
+			}
+			counter--;
+			var inputs = $(".pair");
+			 $(inputs[inputs.length - 1]).remove();
+		});
+		
+});	
