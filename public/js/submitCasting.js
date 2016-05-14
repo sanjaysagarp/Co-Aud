292424508e9117b540586ab66081f09b969362d5
Sean Cheong <your_email@example.com>
2016-05-13 17:58:38 -0700

    audition form validation

--- public/js/submitCasting.js
+++ public/js/submitCasting.js
@@ -12,10 +12,38 @@ function validateText(id) {
 		div.addClass("has-success");
 		div.addClass("has-feedback");
 		$("#glypcn"+id).remove();
-		div.append('<span id="glypcn'+id +'" class="glyphicon glyphicon-ok form-control-feedback" style=display:inline;></span>');
+		div.append('<span id="glypcn'+id +'" class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+		div.append('<span id="inputSuccess4Status" class="sr-only">(success)</span>');
 		return true;
 	}
 }
+
+// function validateForm() {
+// 	var radios = document.getElementsByName("gender");
+// 	var formValid = false;
+// 	var i = 0;
+// 	while (!formValid && i < radios.length) {
+// 		if (radios[i].checked) {
+// 			formValid = true;
+// 			i++;  	 
+// 			var div = $("#genderRadio");    
+// 			div.removeClass("has-error");
+// 			div.addClass("has-success");
+// 			div.addClass("has-feedback");
+// 			$("#glypcn"+id).remove();
+// 			div.append('<span id="glypcn'+id +'" class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+// 			div.append('<span id="inputSuccess4Status" class="sr-only">(success)</span>');
+// 		}
+// 	}
+// 	if (!formValid) {
+// 		var div = $("#genderRadio");
+// 		div.removeClass("has=success");
+// 		$("#glypcn"+id).remove();
+// 		div.addClass("has-error has-feedback");
+// 		div.append('<span id="glypcn2" class="glyphicon glyphicon-remove form-control-feedback"></span>');
+// 		return formValid;
+// 	} 
+// }
 $(document).ready(
 	function(){
 		$("#castSubmission").click(function() {
@@ -38,7 +66,10 @@ $(document).ready(
 			if(validateText("script")) {
 				checker++;
 			}
-			if(checker == 6) {
+			// if(validateForm()) {
+			// 	checker++;
+			// }
+			if(checker == 7) {
 				$("form#castingForm").submit();
 			} else {
 				return false;
